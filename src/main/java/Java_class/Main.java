107887fb812b4367455136735ee1f0c37ba51bc6
Liron <lironuser@gmail.com>
2023-05-30 19:01:20 +0300

    5/30/2023 - 18:53

--- src/main/java/Java_class/Main.java
+++ src/main/java/Java_class/Main.java
@@ -1,10 +1,7 @@
 package Java_class;
 
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
-        User_info userInfo = new User_info();
-        Password password = new Password();
     }
 }
